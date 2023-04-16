80ce6c4b310c6c073b2c59cd4fbd99fe1c80f2a2
watariRyo <d.silv.vc1919@gmail.com>
2023-04-16 20:00:20 +0900

    fix select box bug

--- client/src/routes/balance/+page.svelte
+++ client/src/routes/balance/+page.svelte
@@ -170,7 +170,7 @@
         
             <div class="sm:col-span-2">
                 <label for="tag" class="mb-2 inline-block text-sm text-gray-800 sm:text-base">Tag</label>
-                <select name="tag" on:change={balanceTagChange} class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring">
+                <select name="tag" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring">
                     {#each balanceTags as balanceTag}
                         <option value={balanceTag.id}>{balanceTag.tag_name}</option>
                     {/each}
